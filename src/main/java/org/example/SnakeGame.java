4e2ed99f8a1615b7220ec1b85b28ebdfab902da1
Madumarov Davron <menaceteacher@gmail.com>
2025-01-30 01:52:45 +0500

    add

--- src/main/java/org/example/SnakeGame.java
+++ src/main/java/org/example/SnakeGame.java
@@ -8,7 +8,23 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class SnakeGame extends JPanel implements ActionListener, KeyListener {
-   private final int TILE_SIZE = 25;
+    private final int TILE_SIZE = 25;
+    private final int WIDTH = 20;
+    private final int HEIGHT = 20;
+    private final int SCREEN_WIDTH = WIDTH * TILE_SIZE;
+    private final int SCREEN_HEIGHT = HEIGHT * TILE_SIZE;
+
+    private final int[] x = new int[WIDTH * HEIGHT];
+    private final int[] y = new int[WIDTH * HEIGHT];
+
+    private int bodyParts = 5;
+    private int applesEaten;
+    private int appleX;
+    private int appleY;
+
+    private char direction = 'R';
+    private boolean running = false;
+    private Timer timer;
 
     public static void main(String[] args) {
 
