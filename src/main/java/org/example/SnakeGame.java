14f385a1c8c826ec12bf01e06f368d5b4b3ba736
Madumarov Davron <menaceteacher@gmail.com>
2025-01-30 01:50:58 +0500

    initial commit

---
+++ src/main/java/org/example/SnakeGame.java
@@ -0,0 +1,36 @@
+package org.example;
+
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class SnakeGame extends JPanel implements ActionListener, KeyListener {
+   private final int TILE_SIZE = 25;
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
