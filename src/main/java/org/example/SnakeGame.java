df82203e6c22598e8046f06edb2d50e6016287c9
Madumarov Davron <menaceteacher@gmail.com>
2025-01-30 01:55:13 +0500

    add

--- src/main/java/org/example/SnakeGame.java
+++ src/main/java/org/example/SnakeGame.java
@@ -2,6 +2,7 @@ package org.example;
 
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -25,6 +26,51 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
     private char direction = 'R';
     private boolean running = false;
     private Timer timer;
+
+    public SnakeGame() {
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(this);
+        startGame();
+    }
+
+    private void startGame() {
+        newApple();
+        running = true;
+        timer = new Timer(100, this);
+        timer.start();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        if (running) {
+            // Apple
+            g.setColor(Color.RED);
+            g.fillOval(appleX, appleY, TILE_SIZE, TILE_SIZE);
+            for (int i = 0; i < bodyParts; i++) {
+                if (i == 0) {
+                    g.setColor(Color.GREEN);
+                } else {
+                    g.setColor(new Color(45, 180, 0));
+                }
+                g.fillRect(x[i], y[i], TILE_SIZE, TILE_SIZE);
+            }
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("Arial", Font.BOLD, 18));
+            g.drawString("Score: " + applesEaten, 10, 20);
+        } else {
+            gameOver(g);
+        }
+    }
+
+    private void newApple() {
+    }
+
 
     public static void main(String[] args) {
 
